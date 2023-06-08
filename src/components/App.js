3b6c21c05649ea01056e47c0693c3b9ca06639b6
manishjaunjalkar <manishvjaunjalkar@gmail.com>
2023-06-09 00:37:50 +0530

    comited solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time, setTime] = useState(0)
+
+    useEffect(()=>{
+      const idx = setInterval(()=>{
+        setTime((prev)=>prev+1)
+      },1000)
+  
+    return () => {
+     clearInterval(idx)
+    }
+  }, [])
+  
+  
+  console.log(time)
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>You have on this page from {time} seconds</h1>
     </div>
   )
 }
